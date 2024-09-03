6640e21e3d9b5c24d053cd4a9c07b59ab91b92d2
leolice36 <leoliceanaelmojica@gmail.ccom>
2024-09-04 04:11:27 +0800

    Adds humanScore and computerScore variables
    initialized at 0

--- rock.js
+++ rock.js
@@ -7,7 +7,8 @@ function getHumanChoice(choice){
     if (choice == 1){console.log("rock")}
     else if (choice == 2){console.log("paper")}
     else if (choice == 3){console.log("scissors")}
-    else (console.log("Enter a value from 1 to 3"))
-    return 
+    else (console.log("Enter a value from 1 to 3")) 
 }
 
+let humanScore = 0
+let computerScore = 0
