174bc7d6359579f50cec5416d4070b97c53fb26d
leolice36 <leoliceanaelmojica@gmail.ccom>
2024-09-06 04:18:31 +0800

    Adds getComputerChoice and playRound functions
    getComputerChoice => returns rock paper scissors values from pseudoRNG values
    playRound =>
        *compares computerChoice and humanChoice
        *increments scores

--- rock.js
+++ rock.js
@@ -24,26 +24,20 @@ function getHumanChoice(){
         }
     }
 }
-let humanChoiceFinal = getHumanChoice()
-console.log(humanChoiceFinal)
-
-function convertToSeed(humanChoiceFinal){
-    if (humanChoiceFinal === "ROCK"){
+function convertToSeed(rawChoice){
+    if (rawChoice === "ROCK"){
         return 1;
     }
-    else if (humanChoiceFinal === "PAPER"){
+    else if (rawChoice === "PAPER"){
         return 2;
     }
-    else if (humanChoiceFinal === "SCISSORS"){
+    else if (rawChoice === "SCISSORS"){
         return 3;
     }
-    else if (humanChoiceFinal === "CANCELED") {
+    else if (rawChoice === "CANCELED") {
         return undefined
     }
 }
-
-let choiceConverted = convertToSeed(humanChoiceFinal)
-console.log(choiceConverted)
 
 function pseudoRNG(seed) {
     if (seed === undefined){
@@ -73,53 +67,75 @@ function pseudoRNG(seed) {
         // 31% chance to return something other than seed or "beat"
         else {
             let options = [1, 2, 3].filter(num => num !== seed && num !== (seed % 3) + 1);
-            return function pseudoRNG(seed) {
-                if (seed === undefined){
-                    return console.log("WHAT A BITCH")
-                }
-                else if (![1, 2, 3].includes(seed)) {
-                    throw new Error("Invalid seed. Seed must be 1, 2, or 3.");
-                }
-
-                else {
-
-                // Generate a random number between 0 and 1
-                const rand = Math.random();
-
-                // 33% chance to match the seed
-                if (rand < 0.33) {
-                    return seed;
-                }
-
-                // 36% chance to "beat" the seed
-                else if (rand < 0.69) {
-                    // Beat logic: 1 beats 3, 2 beats 1, 3 beats 2
-                    if (seed === 1) return 2;
-                    if (seed === 2) return 3;
-                    if (seed === 3) return 1;
-                }
-
-                // 31% chance to return something other than seed or "beat"
-                else {
-                    let options = [1, 2, 3].filter(num => num !== seed && num !== (seed % 3) + 1);
-                    return options[Math.floor(Math.random()*options.lenght)];
-                };
-                }
-            }
+            return options[Math.floor(Math.random()*options.length)];
         }
     }
 }
 
-console.log(pseudoRNG(choiceConverted))
-
 function getComputerChoice(){
-    return Math.random()
+    
+    if (humanChoiceFinal === undefined )
+        console.log("WHAT A BITCH");
+    else {
+        let humanChoiceConverted = convertToSeed(humanChoiceFinal)
+        console.log({humanChoiceConverted})
+        let computerChoice = pseudoRNG(humanChoiceConverted)
+        console.log({computerChoice})
+        if (computerChoice === undefined) {
+            return "CANCELED";
+        }
+
+        if (computerChoice == 1){
+            console.log("Computer: ROCK");
+            return "ROCK";
+        } else if (computerChoice == 2) {
+            console.log("Computer: PAPER");
+            return "PAPER";
+        } else if (computerChoice == 3) {
+            console.log("Computer: SCISSORS");
+            return "SCISSORS";
+        }
+
+        return computerChoice
+    }
 }
 
-
-
-
-
+// runs both but getComputerChoice depends on humanChoiceFinal value
+// let humanChoiceFinal = getHumanChoice()
+// let computerChoiceFinal = getComputerChoice()
 
 let humanScore = 0
 let computerScore = 0
+
+function playRound(humanChoice, computerChoice){
+    if (humanChoice === computerChoice){
+        console.log("AGAIN!")
+    }
+    else if(
+        (humanChoice === "ROCK" && computerChoice === "SCISSORS")
+        || (humanChoice === "SCISSORS" && computerChoice === "PAPER")
+        || (humanChoice === "PAPER" && computerChoice === "ROCK")
+        ){
+        console.log("HUMAN WIN!")
+        return humanScore++   
+    }
+    else{
+        console.log("HUMAN LOSE!")
+        return computerScore++  
+    }
+}
+
+let x = "ROCK"
+let y = "PAPER"
+let z = "SCISSORS"
+
+
+playRound(x,z)
+playRound(x,z)
+playRound(x,z)
+playRound(x,z)
+playRound(x,y)
+playRound(x,y)
+playRound(x,y)
+console.log({humanScore})
+console.log({computerScore})
